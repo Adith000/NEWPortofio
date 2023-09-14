e4f70d5a40968a90566c6ca3d9b4fc828a653f59
Adith000 <adithkp000@gmail.com>
2023-09-14 22:52:21 +0530

    Comitting

---
+++ app.js
@@ -0,0 +1,21 @@
+const  express =require("express") ;
+const  dotenv =require("dotenv") ;
+const  path =require("path") ;
+
+
+dotenv.config();
+
+const app =express();
+
+ //Portfolio\dist
+ //Portfolio\index.html
+app.use(express.static(path.join( __dirname,'./Portfolio')))
+app.use('*',function(req,res){
+    res.sendFile(path.join( __dirname,'/index.html'))
+})
+
+
+const PORT =process.env.PORT;
+
+app.listen(PORT,()=>{console.log(`Server is up oand Runnging on ${process.env.PORT}`);
+})
